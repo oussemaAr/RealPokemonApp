c2e163ede8af50f2cef740a11433273b75b44edb
oussemaAr <oussema.a@turing.com>
2022-03-23 12:42:59 +0100

    swipe to refresh

--- app/src/main/java/com/digitals/realpokemonapp/MainActivity.kt
+++ app/src/main/java/com/digitals/realpokemonapp/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.digitals.realpokemonapp.adapter.PokemonAdapter
 import com.digitals.realpokemonapp.databinding.ActivityMainBinding
 import com.digitals.realpokemonapp.mock.pokemonList
+import com.digitals.realpokemonapp.model.Pokemon
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,7 +25,24 @@ class MainActivity : AppCompatActivity() {
             adapter = pokemonAdapter
         }
         pokemonAdapter.submitList(pokemonList)
+        initTouchListener(pokemonAdapter)
 
+        binding.pokemonSwipeToRefresh.setOnRefreshListener {
+            pokemonList.add(
+                0,
+                Pokemon(
+                    "Squirtle",
+                    "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png",
+                    R.color.teal_700
+                )
+            )
+            pokemonAdapter.notifyItemInserted(0)
+            binding.pokemonSwipeToRefresh.isRefreshing = false
+            binding.pokemonRecyclerView.smoothScrollToPosition(0)
+        }
+    }
+
+    private fun initTouchListener(pokemonAdapter: PokemonAdapter) {
         val touchHelper = object : ItemTouchHelper.SimpleCallback(
             ItemTouchHelper.UP or ItemTouchHelper.DOWN or ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT,
             ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
