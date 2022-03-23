75d9ec27af9213967113f6e77069bc48cc1ff465
oussemaAr <oussema.a@turing.com>
2022-03-23 12:30:47 +0100

    Adding swipe to delete drag

--- app/src/main/java/com/digitals/realpokemonapp/MainActivity.kt
+++ app/src/main/java/com/digitals/realpokemonapp/MainActivity.kt
@@ -3,6 +3,8 @@ package com.digitals.realpokemonapp
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
 import com.digitals.realpokemonapp.adapter.PokemonAdapter
 import com.digitals.realpokemonapp.databinding.ActivityMainBinding
 import com.digitals.realpokemonapp.mock.pokemonList
@@ -22,5 +24,31 @@ class MainActivity : AppCompatActivity() {
             adapter = pokemonAdapter
         }
         pokemonAdapter.submitList(pokemonList)
+
+        val touchHelper = object : ItemTouchHelper.SimpleCallback(
+            ItemTouchHelper.UP or ItemTouchHelper.DOWN or ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT,
+            ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+        ) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                val oldPosition = viewHolder.adapterPosition
+                val newPosition = target.adapterPosition
+                pokemonList.add(newPosition, pokemonList[oldPosition])
+                pokemonList.removeAt(oldPosition)
+                pokemonAdapter.notifyItemMoved(newPosition, oldPosition)
+                return true
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                pokemonList.removeAt(position)
+                pokemonAdapter.notifyItemRemoved(position)
+            }
+        }
+
+        ItemTouchHelper(touchHelper).attachToRecyclerView(binding.pokemonRecyclerView)
     }
 }
--- app/src/main/java/com/digitals/realpokemonapp/mock/PokemonMock.kt
+++ app/src/main/java/com/digitals/realpokemonapp/mock/PokemonMock.kt
@@ -3,7 +3,7 @@ package com.digitals.realpokemonapp.mock
 import com.digitals.realpokemonapp.R
 import com.digitals.realpokemonapp.model.Pokemon
 
-val pokemonList = listOf(
+val pokemonList = mutableListOf(
     Pokemon(
         "Bulbasaur",
         "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
@@ -21,17 +21,17 @@ val pokemonList = listOf(
     ),
     Pokemon(
         "Charmander",
-        "https://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png",
+        "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png",
         R.color.teal_700
     ),
     Pokemon(
         "Charmeleon",
-        "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png",
+        "https://assets.pokemon.com/assets/cms2/img/pokedex/full/005.png",
         R.color.teal_700
     ),
     Pokemon(
         "Charizard",
-        "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png",
+        "https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png",
         R.color.teal_700
     ),
 )
